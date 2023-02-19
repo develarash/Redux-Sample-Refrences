b4f5e4979c8c308a046826d8d5661d741a7fa2b0
arashRamy <develarash@gmail.com>
2023-02-19 21:44:52 +0330

    first 25 commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
 let lastId = 0;
 // reducer is a pure function
-function reducer(state = [], action) {
+export default function reducer(state = [], action) {
   switch (action.type) {
     case "bugAdded":
       return [

6ce5058699cd697cc955ca328c64e55ec06421de
arashRamy <develarash@gmail.com>
2023-02-24 17:30:31 +0330

    commited

--- src/customStore.js
+++ src/customStore.js
@@ -1,12 +1,16 @@
-function createStore() {
+import reducer from "./reducer";
+function createStore(reducer) {
   let state;
-
+  function dispatch(action) {
+    state = reducer(state, action);
+  }
   function getState() {
     return state;
   }
 
   return {
     getState,
+    dispatch,
   };
 }
-export default createStore();
+export default createStore(reducer);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import store from "./customStore";
-store.state = 1;
-console.log(store);
+import * as actions from "./action";
+store.dispatch(actions.bugAdded("bug first"));
+
+console.log(store.getState());
